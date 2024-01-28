a1d8a0a03e24b816bca49dad585cac9ca9b75225
Lenok <olena.andriushchenko@powercoders.org>
2024-01-28 22:42:41 +0100

    edit mainlayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,75 +1,69 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar class="justify-between">
-        <q-btn
-          flat
-          dense
-          round
-          class="hidden"
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-        <q-btn
-          v-if="$route.path !== '/'"
-          flat
-          dense
-          round
-          icon="arrow_back"
-          label="Back"
-          @click="goBack"
-        />
+  <template>
+    <q-layout view="lHh Lpr lFf">
+      <q-header elevated>
+        <q-toolbar class="justify-between">
+          <q-btn
+            flat
+            dense
+            round
+            class="hidden"
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          />
+          <q-btn
+            v-if="$route.path !== '/'"
+            flat
+            dense
+            round
+            icon="arrow_back"
+            label="Back"
+            @click="goBack"
+          />
 
-        <q-toolbar-title v-if="isAuthenticated">
-          {{ $route.name + " - I sent " + messagesFromMeCount + " messages" }}
-        </q-toolbar-title>
+          <q-toolbar-title v-if="isAuthenticated">
+            {{ $route.name + " - I sent " + messagesFromMeCount + " messages" }}
+          </q-toolbar-title>
 
-        <q-btn
-          class="q-pr-lg"
-          flat
-          dense
-          round
-          icon="account_circle"
-          :label="name == ''?  'login': name"
-          to="/"
-        />
-      
-      </q-toolbar>
-    </q-header>
+          <q-btn
+            class="q-pr-lg"
+            flat
+            dense
+            round
+            icon="account_circle"
+            :label="name == ''?  'login': name"
+            to="/"
+          />
+        
+        </q-toolbar>
+      </q-header>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
-</template>
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </template>
 
-<script>
-import { defineComponent } from "vue";
+  <script>
+  import { defineComponent } from "vue";
 
-import { mapState } from "pinia";
-import { useStore } from "src/stores/store";
-
-
+  import { mapState } from "pinia";
+  import { useStore } from "src/stores/store";
 
 
-export default defineComponent({
-  name: "MainLayout",
 
-  computed: {
-    ...mapState(useStore, ["messagesFromMeCount", "name", "isAuthenticated"]),
-  },
 
-  setup() {
-
-    return {
-
-    };
-  },
-  methods: {
-    goBack() {
-      this.$router.go(-1);
+  export default defineComponent({
+    name: "MainLayout",
+    computed: {
+      ...mapState(useStore, ["messagesFromMeCount", "name", "isAuthenticated"]),
     },
-  },
-});
-</script>
+
+   
+    methods: {
+      goBack() {
+        this.$router.go(-1);
+      },
+    },
+  });
+  </script>
